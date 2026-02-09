dc4eb9f52945ba48355b6c7c8a79986c7c452559
Mohcine Tor <mohcine.tor@cosmotech.com>
2026-02-09 16:27:32 +0100

    add groups mapper to clinet web & enbale service accounts to client api

--- modules/config_keycloak_realm/main.tf
+++ modules/config_keycloak_realm/main.tf
@@ -131,6 +131,7 @@ resource "keycloak_openid_client" "cosmotech_api" {
   root_url              = local.root_url
   base_url              = local.base_url
   valid_redirect_uris   = local.valid_redirect_uris
+  service_accounts_enabled = true
 
 
   depends_on = [
@@ -175,6 +176,24 @@ resource "keycloak_generic_protocol_mapper" "mapper_cosmotech_web" {
     "introspection.token.claim" : "true"
   }
 }
+
+resource "keycloak_generic_protocol_mapper" "mapper_cosmotech_web_groups" {
+  realm_id        = keycloak_realm.realm.id
+  client_id       = keycloak_openid_client.cosmotech_web.id
+  name            = "groups"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usermodel-realm-role-mapper"
+  config = {
+    "id.token.claim" : "true",
+    "access.token.claim" : "true",
+    "claim.name" : "groups",
+    "jsonType.label" : "String",
+    "multivalued" : "true",
+    "userinfo.token.claim" : "true",
+    "introspection.token.claim" : "true"
+  }
+}
+
 # --- Client cosmotech-client-web ---
 
 
