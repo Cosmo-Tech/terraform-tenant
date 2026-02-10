29e5994feec31601cd56a94ae5ae9b5836f099c6
Mohcine Tor <mohcine.tor@cosmotech.com>
2026-02-10 15:37:26 +0100

    add missing Keycloak config for cosmotech_api clinet

--- modules/config_keycloak_realm/main.tf
+++ modules/config_keycloak_realm/main.tf
@@ -138,6 +138,29 @@ resource "keycloak_openid_client" "cosmotech_api" {
     keycloak_realm.realm,
   ]
 }
+
+resource "keycloak_generic_protocol_mapper" "mapper_cosmotech_api" {
+  realm_id        = keycloak_realm.realm.id
+  client_id       = keycloak_openid_client.cosmotech_api.id
+  name            = "realm roles"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usermodel-realm-role-mapper"
+  config = {
+    "id.token.claim" : "true",
+    "access.token.claim" : "true",
+    "claim.name" : "userRoles",
+    "jsonType.label" : "String",
+    "multivalued" : "true",
+    "userinfo.token.claim" : "true",
+    "introspection.token.claim" : "true"
+  }
+}
+
+resource "keycloak_openid_client_service_account_realm_role" "cosmotech_api_service_account_role" {
+  realm_id                = keycloak_realm.realm.id
+  service_account_user_id = keycloak_openid_client.cosmotech_api.service_account_user_id
+  role                    = keycloak_role.platform_admin.name
+}
 # --- Client cosmotech-client-api ---
 
 
