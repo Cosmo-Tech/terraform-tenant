df883f508bcd4e1c0d0f614b6c6f623fe72d2934
ggtrd <103002419+ggtrd@users.noreply.github.com>
2026-05-22 13:15:16 +0200

    better organization for default variables

--- variables_defaults.tf
+++ variables_defaults.tf
@@ -6,8 +6,8 @@ variable "image_registry_auth_secret" { default = "registry-auth-cgrdev" }
 
 
 # PostgreSQL
-variable "postgresql_chart_repository" { default = "oci://cgr.dev/cosmotech/iamguarded-charts" }
 variable "postgresql_chart_name" { default = "postgresql" }
+variable "postgresql_chart_repository" { default = "oci://cgr.dev/cosmotech/iamguarded-charts" }
 variable "postgresql_chart_tag" { default = "17.1.0" }
 variable "postgresql_storage_size" { default = 8 }
 variable "postgresql_image_repository" { default = "cosmotech/postgres-iamguarded" } # Note: not only used in postgresql module, but also in others modules to ensure psql is the same everywhere
@@ -15,21 +15,21 @@ variable "postgresql_image_tag" { default = "17" }                              
 
 
 # SeaweedFS
-variable "seaweedfs_chart_repository" { default = "oci://cgr.dev/cosmotech/iamguarded-charts" }
 variable "seaweedfs_chart_name" { default = "seaweedfs" }
+variable "seaweedfs_chart_repository" { default = "oci://cgr.dev/cosmotech/iamguarded-charts" }
 variable "seaweedfs_chart_tag" { default = "6.0.4" }
 variable "seaweedfs_storage_size" { default = 32 }
 
 
 # Argo Workflows
-variable "argo_chart_repository" { default = "oci://cgr.dev/cosmotech/iamguarded-charts" }
 variable "argo_chart_name" { default = "argo-workflows" }
+variable "argo_chart_repository" { default = "oci://cgr.dev/cosmotech/iamguarded-charts" }
 variable "argo_chart_tag" { default = "13.0.6" }
 
 
 # Redis
-variable "redis_chart_repository" { default = "oci://cgr.dev/cosmotech/iamguarded-charts" }
 variable "redis_chart_name" { default = "redis" }
+variable "redis_chart_repository" { default = "oci://cgr.dev/cosmotech/iamguarded-charts" }
 variable "redis_chart_tag" { default = "25.3.8" }
 variable "redis_storage_size" { default = 16 }
 variable "redis_image_repository" { default = "cosmotech/redis-server-iamguarded" }
@@ -37,6 +37,6 @@ variable "redis_image_tag" { default = "8.6.3" }
 
 
 # Cosmo Tech API
-variable "cosmotechapi_chart_repository" { default = "https://cosmo-tech.github.io/helm-charts" }
 variable "cosmotechapi_chart_name" { default = "cosmotech-api" }
+variable "cosmotechapi_chart_repository" { default = "https://cosmo-tech.github.io/helm-charts" }
 variable "cosmotechapi_chart_tag" { default = "5.0.1" }
